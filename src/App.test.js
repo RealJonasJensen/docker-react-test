f07a570d1e97acc65c5d1a226c3069372460d866
RealJonasJensen <mrabkjaer@gmail.com>
2019-11-29 17:30:32 +0100

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,15 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders withsssout crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders sdsdsd crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
